7c34e38850d3933c03b377e1b10865167b6749a8
Maciej Krogulski <maciejkrogulski1@gmail.com>
2022-09-27 21:28:12 +0200

    first task finished

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,23 @@
 const categories = document.getElementById('categories')
 
-const numberOfCategories = categories.children.length;
+
+const number = categories.children.length;
+
+const numberOfCategories = `Number of categories: ${number}`
 
 console.log(numberOfCategories)
+
+const item = document.querySelectorAll('.item')
+
+const itemName = document.querySelectorAll('.item h2')
+
+const itemElements = document.querySelectorAll('.item ul')
+
+item.forEach(function(element) {
+    const itemh2 = element.querySelector('h2')
+    const itemElementsNumber = element.querySelector('ul')
+    
+    console.log(`Category: ${itemh2.textContent}
+Elements: ${itemElementsNumber.children.length}`)
+}
+)
