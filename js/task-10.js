1271215a646d38c666ff749db4ab2070fe681244
Maciej Krogulski <maciejkrogulski1@gmail.com>
2022-10-04 18:48:16 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,45 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonCreate = document.querySelector("button[data-create]");
+
+const buttonDestroy = document.querySelector("button[data-destroy]");
+
+const divBoxes = document.getElementById("boxes");
+
+const input = document.querySelector("input");
+
+let amount = 0;
+
+input.addEventListener("input", () => {
+  amount = input.value;
+});
+
+buttonCreate.addEventListener("click", createBoxes);
+
+buttonDestroy.addEventListener("click", destroyBoxes);
+
+let pixels = 30
+
+function createBoxes() {
+
+  for (let i = 0; i < amount; i++) {
+
+    if (amount === 0) {
+      return;
+    }
+    let a = 0;
+    pixels = pixels + (i * 10);
+    a = document.createElement("div");
+    divBoxes.append(a);
+    a.style.backgroundColor = getRandomHexColor();
+
+    a.style.width = `${pixels}px`;
+    a.style.height = `${pixels}px`;
+  }
+}
+
+function destroyBoxes() {
+  divBoxes.textContent = "";
+}
