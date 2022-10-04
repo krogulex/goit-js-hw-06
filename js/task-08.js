8ccc9c77697a6a11bfd55ee8dcc3c61a60f0da10
Maciej Krogulski <maciejkrogulski1@gmail.com>
2022-10-04 14:06:04 +0200

    task9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form')
 
+function login(event) {
+
+    event.preventDefault();
+
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+
+      if (email.value === "" || password.value === "") {
+        return console.log("Please fill in all the fields!");
+      }
+      console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+      event.currentTarget.reset();
+
+    }
+
+form.addEventListener('submit', login)
